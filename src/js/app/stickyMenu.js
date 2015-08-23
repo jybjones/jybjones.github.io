552a33c4aa925c92b703097358a91428cf914271
Jennifer Jones <jybjones@gmail.com>
2015-08-23 17:58:22 -0500

    initial layout

---
+++ src/js/app/stickyMenu.js
@@ -0,0 +1,10 @@
+// $(window).on('scroll', function() {
+//   var scroll     = $(window).scrollTop(),
+//     feature    = $('#feature'),
+//     primaryNav = $('#navbar-default navbar-fixed-top');
+
+//   if (scroll >= feature.outerHeight())
+//     primaryNav.addClass('sticky');
+//   else
+//     primaryNav.removeClass('sticky');
+// });
